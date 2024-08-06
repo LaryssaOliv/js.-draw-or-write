b16a7abf785c1a4e50652aa59ec903cb55814a1d
LaryssaOliv <laryssalupi16@gmail.com>
2024-08-06 09:41:59 -0300

    Adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("red");
+    fill("yellow");
+    
+    if(mouseIsPressed){
+    rect(mouseX, mouseY, 30, 45);
+    }
+  }
